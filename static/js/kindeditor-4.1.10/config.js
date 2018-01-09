bac12656e080e0cc2357e2b9e4dd805fda55acc0
cuitongji <ctj123@sohu.com>
2018-01-09 14:07:28 +0800

    configure upload

--- static/js/kindeditor-4.1.10/config.js
+++ static/js/kindeditor-4.1.10/config.js
@@ -1,7 +1,6 @@
 KindEditor.ready(function(K) {
                 K.create('textarea[name=content]',{
-                    width:'800px',
-                    height:'200px',
-                    uploadJson: '/admin/upload/kindeditor',
+                    width:800,
+                    height:200,
                 });
         });
